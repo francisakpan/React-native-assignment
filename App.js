d7382ab7c7fa53608d680f2c757d340727498e4d
Francis Akpan <francisf.akpan@gmail.com>
2021-04-14 17:29:56 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View , SafeAreaView} from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [tem, setTemp] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto"  />
+      <View>
+        <Text>{text}</Text>
+      </View>
+
+      <View style={styles.takeDown} >
+        <TextInput 
+          placeholder = "Enter a text here..."
+          style={styles.input} 
+          onChangeText={(e)=>setTemp(e)}
+        />
+        <Button title="Enter" 
+          onPress={()=>setText(text => text + tem + "\n")}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  takeDown: {
+    position: 'absolute',
+    bottom: 40,
+    flexDirection:"row", 
+    justifyContent: "center",
+    alignContent: "center"
+  },
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 16
   },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    width: "80%"
+  }
 });
